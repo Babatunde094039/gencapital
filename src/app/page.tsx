6573a080879be260ff20a2c832313e9df97489e1
Hafolabi <afolabioladipo6@gmail.com>
2024-09-10 18:17:46 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             <p className={`${OpenSauceSans.className} font-[700] 3xl:text-[92px] xl:text-[82px] lg:text-[68px] text-[55px] text-center !text-[#FFFFFF] leading-[70px] md:leading-none`}>
               Intelligent Investing
             </p>
-            <p className="3xl:text-[24px] md:text-[22px] text-[18px] md:w-[48%] xl:w-[626px] w-full text-center font-[100] mt-6 !text-[#FFFFFF]">
+            <p className="3xl:text-[24px] md:text-[22px] text-[18px] md:w-[48%] xl:w-[626px] w-[200px] text-center font-[100] mt-6 !text-[#FFFFFF]">
               Connecting investors to global securities.
             </p>
             <Link href={'#WaitList'} className="2xl:w-[155px] w-[140px] mt-8 flex items-center justify-center 2xl:h-[55px] h-[50px] bg-[#2EB200] transform -skew-x-12 font-[200] rounded-md">
